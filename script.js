c448621dfab6b38433fdaea2187f67c815798dc4
bwill619 <bjw0619@gmail.com>
2022-01-19 00:01:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,2 @@
 const firstName = document.getElementById('firstName');
 const url = "https://balldontlie.io/api/v1/players?search=doncic" 
-
-fetch(url)
-    .then((response) => {
-        return response.json();
-    })
-    .then((data) => {
-        let players = data;
-
-        players.map(function(player){
-            let firstName = document.getElementById("firstName").innerHTML = '{players.first_name}'
-            firstName.appendChild(firstName);
-
-        }); 
-        
-    });

fc848d98b76d1581402af0ab3c0aa96510fd363c
bwill619 <bjw0619@gmail.com>
2022-01-28 00:47:16 -0500

    commit on main

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function searchPlayer(input) {
     .then(response => response.json())
     .then((jsonData) => {
         const array = [jsonData];
-        const playerID = array.map(player => `${player.data[0].id}`);
         const fName = array.map(player => `${player.data[0].first_name}`);
         const lName = array.map(player => `${player.data[0].last_name}`);
         const height = array.map(player => `${player.data[0].height_feet}` + "'" + `${player.data[0].height_inches}`);
@@ -31,28 +30,6 @@ window.onload = () => {
     }
 }
 
-function getPlayerId(input) {
-    const url = `https://balldontlie.io/api/v1/players?search=${input}`;
-    fetch(url)
-    .then(response => response.json())
-    .then((jsonData) => {
-        const array = [jsonData];
-        const playerID = array.map(player => `${player.data[0].id}`);
-    });
-    input = playerID;
-}
-
-function getSeasonAverages(input) {
-    const url2 =  `https://balldontlie.io/api/v1/season_averages?season=2021&player_ids[]=${getPlayerId(input)}`
-    fetch(url2)
-    .then(response2 => response2.json())
-    .then((jsonData2) => {
-        const array2 = [jsonData2];
-        const gamesPlayed = array2.map(season => `${season.data[0].games_played}`);
-
-        document.getElementById("GP").innerHTML = gamesPlayed;
-    });
-}
 
 
 
